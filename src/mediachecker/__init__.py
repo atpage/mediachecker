89558a90fe5c296bcf112490aee232b85fa0a96f
Alex Page <a.t.page@gmail.com>
2022-09-09 11:29:11 -0400

    Add option to save output to a log

--- src/mediachecker/__init__.py
+++ src/mediachecker/__init__.py
@@ -12,7 +12,12 @@ class AVFile:
             raise TypeError('filename must be a string.')
         self.filename = filename
 
-    def is_good(self, method='first_audio_track'):
+    def is_good(self, method='first_audio_track', write_log=False):
+        """write_log will place a log file alongside the input file,
+        containing any output from ffmpeg.  e.g. example.mp4 would
+        generate example.mp4.log.  The log file will be overwritten if
+        it exists!
+        """
         if self.filename is None:
             raise ValueError('No filename was given.')
         if not os.path.isfile(self.filename):
@@ -33,11 +38,14 @@ class AVFile:
                 shell = True,
                 stderr = subprocess.STDOUT,
             )
-            # TODO: save output
             # TODO?: stop immediately when we get output, return False
             # without checking the rest of the file?  (-xerror should
             # work for this, but will be caught below.)
         except subprocess.CalledProcessError:
             warn("Couldn't process %s" % self.filename)
             return None
+        if write_log:
+            log_filename = self.filename + '.log'
+            with open(log_filename, 'wb') as f:
+                f.write(output)
         return len(output) == 0
