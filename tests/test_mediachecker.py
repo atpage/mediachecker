08beaafa88f9d6fed2501510bf4171d7584f5335
Alex Page <a.t.page@gmail.com>
2022-09-10 17:25:01 -0400

    Add tests

--- tests/test_mediachecker.py
+++ tests/test_mediachecker.py
@@ -8,25 +8,26 @@ tests_dir = Path(__file__).parent
 
 
 class TestMediaChecker(unittest.TestCase):
+    def test_good_file_detection(self):
+        f = AVFile(os.path.join(tests_dir, 'data', 'good_example.mp4'))
+        self.assertTrue(f.is_good(method='first_audio_track'))
+        self.assertTrue(f.is_good(method='full'))
 
-    # def test_good_file_detection(self):
-    #     f = AVFile(os.path.join(tests_dir, 'data', 'good_example.mp4'))
-    #     self.assertTrue(f.is_good())
-    #     self.assertTrue(f.is_good(method='full'))
+    def test_bad_file_detection(self):
+        f = AVFile(os.path.join(tests_dir, 'data', 'bad_example.mp4'))
+        self.assertFalse(f.is_good(method='first_audio_track'))
+        self.assertFalse(f.is_good(method='full'))
 
-    # def test_bad_file_detection(self):
-    #     f = AVFile(os.path.join(tests_dir, 'data', 'bad_example.mp4'))
-    #     self.assertFalse(f.is_good())
-    #     self.assertFalse(f.is_good(method='full'))
-
-    # def test_tricky_bad_file_detection(self):
-    #     f = AVFile(os.path.join(tests_dir, 'data', 'tricky_example.mp4'))
-    #     self.assertFalse(f.is_good(method='full'))
+    def test_tricky_bad_file_detection(self):
+        f = AVFile(os.path.join(tests_dir, 'data', 'tricky_example.mp4'))
+        self.assertFalse(f.is_good(method='full'))
 
     def test_invalid_file_type(self):
         f = AVFile(os.path.join(tests_dir, 'data', 'invalid_example.txt'))
         with self.assertWarns(UserWarning) as cm:
-            self.assertIsNone(f.is_good())
+            self.assertIsNone(f.is_good(method='first_audio_track'))
+        with self.assertWarns(UserWarning) as cm:
+            self.assertIsNone(f.is_good(method='full'))
 
     # TODO:
     # * test file with weird characters in the name
